ee88112b6a0fa721cb3d945110c8ad2cd49325c9
show <1907109855@qq.com>
2021-12-18 22:54:09 +0800

    向 Rvar 添加 uniquify pass

---
+++ Assig-2/RvarUniquifyPass.hs
@@ -0,0 +1,44 @@
+module RvarUniquifyPass where
+
+import Rvar
+    ( Program(Program),
+      Exp(..),
+      Var,
+      Primop(Add, LRead, Neg),
+      program )
+import qualified Data.Map as Map
+
+isUndif :: [Char] -> Maybe p -> p
+isUndif var (Just x) = x
+isUndif var Nothing = error ("undefined variable" ++ var)
+
+gensym :: Show a => [Char] -> a -> [Char]
+gensym var id = var ++ show id
+
+-- data Exp = LInt Int 
+--     | Prim Primop [Exp] 
+--     | Var Var
+--     | Let Var Exp Exp deriving(Show)
+    
+-- data Program info = Program info Exp deriving(Show)
+
+-- 错误，varId并不唯一
+uniquifyExp :: (Show b, Num b) => Map.Map Var Var -> Exp -> b -> (Exp, b)
+uniquifyExp env exp varId = case exp of
+    (LInt x) -> (LInt x, varId)
+    (Var var) -> (Var (isUndif var (Map.lookup var env)), varId)
+    (Let var exp1 exp2) -> let newVar = gensym var varId in
+                               let (e1, newId1) = uniquifyExp env exp1 (varId + 1) in
+                                   let (e2, newId2) = uniquifyExp (Map.insert var newVar env) exp2 newId1 in
+                                       (Let newVar e1 e2, newId2)
+    (Prim LRead []) -> (Prim LRead [], varId)
+    (Prim Neg [exp]) -> let (e1, newId) = uniquifyExp env exp varId in
+                            (Prim Neg [e1], newId)
+    (Prim Add [exp1, exp2]) -> let (e1, newId1) = uniquifyExp env exp1 varId in
+                                   let (e2, newId2) = uniquifyExp env exp2 newId1 in
+                                       (Prim Add [e1, e2], newId2)
+    _ -> error "Undefined behavior"
+
+uniquify :: Program info -> Program info
+uniquify (Program info exp) = let (exp1, _) = uniquifyExp Map.empty exp 0 in
+                                  Program info exp1
--- Assig-2/test.hs
+++ Assig-2/test.hs
@@ -1,4 +1,6 @@
 import RvarInterp (interp)
+import Rvar (program)
+import RvarUniquifyPass (uniquify)
 
 assert :: Bool -> String -> String -> IO ()
 assert test passStatement failStatement = if test
@@ -6,11 +8,11 @@ assert test passStatement failStatement = if test
     else putStrLn failStatement
 
 -- test Rvar interpreter
-
 testInterp :: IO ()
-testInterp = do
-    putStrLn "Running tests Interpreter....."
-    interp "42" >>= \x -> assert (x == 42) "passed '42'" "FAIL '42'"
-    interp "(+ 20 22)" >>= \x -> assert (x == 42) "passed '42'" "FAIL '42'"
-    interp "(let ([x 41]) (+ x 1))" >>= \x -> assert (x == 42) "passed '42'" "FAIL '42'"
+testInterp = let eval = interp . program in do
+                 putStrLn "Running tests Interpreter....."
+                 eval "42" >>= \x -> assert (x == 42) "passed '42'" "FAIL '42'"
+                 eval "(+ 20 22)" >>= \x -> assert (x == 42) "passed '42'" "FAIL '42'"
+                 eval "(let ([x 41]) (+ x 1))" >>= \x -> assert (x == 42) "passed '42'" "FAIL '42'"
     
+-- test uniquify pass 
